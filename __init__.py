4506f5e9b3615dd47c1be272f5b595fc3d557e89
Gayrat Vlasov <ggvlasov@gmail.com>
2025-06-02 21:54:49 +0300

    сократил выход

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 from .GoogleTranslateNode.google_translate_node import GoogleTranslateCLIPTextEncodeNode, GoogleTranslateTextNode
 from .ImageResize.image_scale_by_aspect_ratio_v2 import ImageScaleByAspectRatioV2
 from .ergouzi.EGJDFDHT import EGRYHT
-from .MaskAreaCondition.adaptive_crop_stitch_params_node import AdaptiveCropStitchParamsV2
+from .MaskAreaCondition.adaptive_crop_stitch_params_node import AdaptiveCropStitchParams_Simplified_WithPassthrough
 
 # Список узлов, которые будут зарегистрированы в ComfyUI
 NODE_CLASS_MAPPINGS = {
@@ -11,7 +11,7 @@ NODE_CLASS_MAPPINGS = {
     "GoogleTranslateText": GoogleTranslateTextNode,
     "ImageResize: ImageScaleByAspectRatio V2": ImageScaleByAspectRatioV2,
     "EG_RY_HT" : EGRYHT,
-    "AdaptiveCropStitchParamsV2": AdaptiveCropStitchParamsV2
+    "AdaptiveCropStitchParams_Simplified_WithPassthrough": AdaptiveCropStitchParams_Simplified_WithPassthrough
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -19,5 +19,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "GoogleTranslateText": "Google Translate (Text Only)",
     "ImageResize": "ImageScaleByAspectRatio V2",
     "EG_RY_HT": "Float slider",
-    "AdaptiveCropStitchParamsV2": "Adaptive Crop & Stitch Params (Calc %)"
+    "AdaptiveCropStitchParams_Simplified_WithPassthrough": "Simplified Adaptive Params + Passthrough"
 }
